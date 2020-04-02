e1c5257336734347bafe8909cbd1102ba07aaeba
Calvin Zheng <calvinzheng08@hotmail.com>
2020-04-02 16:05:27 +0000

    assertobjectequal.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,41 @@
+
+
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (key of object1Keys) {
+    if (Array.isArray(object1[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+const object1 = {
+  1: "one",
+  2: "two",
+}
+const object2 = {
+  1: "one",
+  2: "two",
+}
+
+const object3 = {
+  3: "three",
+  2: "two",
+}
+assertObjectsEqual(object1, object2);
+assertObjectsEqual(object1, object3); 
