c58c348db39a2d507894e68d90ce0266b10d0838
Calvin Zheng <calvinzheng08@hotmail.com>
2020-04-06 21:31:53 +0000

    commit

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const tail = function(array) {
   let tailArray = [];
   for (let i = 1; i < array.length; i ++) {
@@ -13,7 +5,4 @@ const tail = function(array) {
   }
   return tailArray;
 };
-/// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
