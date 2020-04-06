6a556f477cd6cb2100fd7ec6a6663a37c6daef85
Calvin Zheng <calvinzheng08@hotmail.com>
2020-04-06 21:43:43 +0000

    commit

--- eqArrays.js
+++ eqArrays.js
@@ -10,18 +10,18 @@
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
 
-function eqArrays(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
-  for (let index = 0; index < a.length; ++index) {
-    if (a[index] !== b[index]) return false;
+  const eqArrays = function(array1, array2) {
+    let result = true;
+    if (array1.length !== array2.length) {
+      result = false;
+    } 
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+    return result;
   }
-  return true;
-}
 
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
